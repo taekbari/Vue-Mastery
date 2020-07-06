c1bd830ae5f07465a2c0520f9477c47615ab1a25
taekbari <taekbari157@gmail.com>
2020-07-06 22:37:31 +0900

    API calls with Axios lecture code

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,15 +1,31 @@
 <template>
     <div>
         <h1>Events Listing</h1>
-        <EventCard />
+        <EventCard v-for="event in events" :event="event" :key="event.id" />
     </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
+
 export default {
     components: {
         EventCard
+    },
+    data() {
+        return {
+            events: null
+        };
+    },
+    created() {
+        EventService.getEvents()
+            .then(response => {
+                this.events = response.data;
+            })
+            .catch(error => {
+                console.log('There was an error : ', error.response);
+            });
     }
 };
 </script>
